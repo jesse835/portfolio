f15d33c94586e914228100f6248df2adfab58c16
Jessé <jesse@communicare.med.br>
2020-08-23 19:37:20 -0300

    Initial SEO config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <main>
+      <Head>
+        <title>My portfolio</title>
+        <meta name="description" content="Content of my site" />
+      </Head>
+      <h2>Test home page</h2>
+    </main>
+  )
+}
