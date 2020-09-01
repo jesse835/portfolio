9eaab411208ebfd6d3a83fb91c5ecb9e500f9a19
Jessé <jesse@communicare.med.br>
2020-09-01 01:07:22 -0300

    Added initial configuration with material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,44 @@
 import '../styles/globals.css'
 
-import Head from 'next/head'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <>
+    <React.Fragment>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
       </Head>
-      <Component {...pageProps} />
-    </>
+      
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
   )
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,35 @@
+import React from 'react'
+import { Button, makeStyles } from '@material-ui/core'
+
 import Head from 'next/head'
 
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+  }
+})
+
 export default function Home() {
+  const style = useStyles()
+
   return (
     <main>
       <Head>
         <title>My portfolio</title>
         <meta name="description" content="Content of my site" />
       </Head>
-      <h2>Test home page</h2>
+      <div className={style.container}>
+        <div>Test home page</div>
+        <br />
+
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
     </main>
   )
 }
